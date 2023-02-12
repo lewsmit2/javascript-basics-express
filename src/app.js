9f330438581824a2e24727a7b903d6f679653f91
Lewis Smith <lewislbssmith212@gmail.com>
2023-02-12 13:05:31 +0000

    error part of track done

--- src/app.js
+++ src/app.js
@@ -42,18 +42,8 @@ app.get('/numbers/add/:num1/and/:num2', (req, res) => {
 app.get('/numbers/subtract/:num1/from/:num2', (req, res) => {
   const a = parseInt(req.params.num2);
   const b = parseInt(req.params.num1);
-
-  res.status(200).json({ result: subtract(a, b) });
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).json({ result: subtract(a, b) });
 });
-
-// app.get('/numbers/add/:num1/and/:num2', (req, res) => {
-//   const a = parseInt(req.params.num1);
-//   const b = parseInt(req.params.num2);
-//   console.log(a);
-//   console.log(b);
-//   return Number.isNaN(a) || Number.isNaN(b)
-//     ? res.status(400).json({ error: 'Parameters must be valid numbers' })
-//     : res.status(200).json({ result: add(a, b) });
-// });
-
 module.exports = app;
